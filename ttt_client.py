633f0e480e8906713dfb9e1266c733b291b7a4cb
CharmySoft <charmysoft@gmail.com>
2016-02-26 20:23:53 +0000

    Support multi-clients at the same time

---
+++ ttt_client.py
@@ -0,0 +1,76 @@
+# Import the socket module
+import socket
+# Import command line arguments
+from sys import argv
+
+# If there are more than 3 arguments 
+if(len(argv) >= 3):
+	# Set the address to argument 1, and port number to argument 2
+	address = argv[1];
+	port_number = argv[2];
+else:
+	# Ask the user to input the address and port number
+	address = input("Please enter the address:");
+	port_number = input("Please enter the port:");
+
+# Create the socket object, the first parameter AF_INET is for IPv4 networking, the second identifies the socket type, SOCK_STREAM is for TCP protocal
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+# Keep repeating connecting to the server
+while True:
+	try:
+		print("Connecting to the game server...");
+
+		# Connect to the specified host and port 
+		client_socket.connect((address, int(port_number)));
+
+		# Break the while loop if no error is caught
+		break;
+
+	except:
+		# Caught an error
+		print("There is an error when trying to connect to ", address, "::", port_number);
+
+		# Ask the user what to do with the error
+		choice = input("[A]bort, [C]hange address and port, or [R]etry?");
+
+		if(choice.lower() == "a"):
+			exit();
+		elif(choice.lower() == "c"):
+			address = input("Please enter the address:");
+			port_number = input("Please enter the port:");
+
+# Print the welcome message from the server
+print(client_socket.recv(1024).decode());
+
+while True:
+	# Prompt the user to enter the message being sent
+	message = input("Please enter the message (q to quit):");
+
+	# Print the message being sent onto the screen
+	print(message);
+
+	# Encode the message being sent
+	data_out = message.encode();
+
+	# Send the message to the server
+	client_socket.send(data_out);
+
+	# If the user enters q, then break the loop and disconnect
+	if(message == "q"):
+		break;
+
+	# Receive message from the server with message size being 1024 bytes
+	data_in = client_socket.recv(1024);
+
+	# Decode the message being received
+	message = data_in.decode();
+
+	# Print the message onto the screen
+	print(message);
+
+# Shut down the socket to prevent further sends/receives
+client_socket.shutdown(socket.SHUT_RDWR);
+
+# Close the socket
+client_socket.close();
---
+++ ttt_server.py
@@ -0,0 +1,76 @@
+# Import the socket module
+import socket
+# Import multi-threading module
+import threading
+# Import command line arguments
+from sys import argv
+
+# If there are more than 2 arguments 
+if(len(argv) >= 2):
+	# Set port number to argument 1
+	port_number = argv[1];
+else:
+	# Ask the user to input port number
+	port_number = input("Please enter the port:");
+
+# Create the socket object, the first parameter AF_INET is for IPv4 networking, the second identifies the socket type, SOCK_STREAM is for TCP protocal
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+# Keep repeating connecting to the server
+while True:
+
+	try:
+		# Bind to an address (localhost) with the designated port 
+		server_socket.bind(("localhost", int(port_number)));
+		print("Reserved port ", port_number);
+
+		# Start listening to the binded address
+		server_socket.listen(1);
+		print("Listening to port ", port_number);
+
+		# Break the while loop if no error is caught
+		break;
+
+	except:
+		# Caught an error
+		print("There is an error when trying to bind localhost::", port_number);
+
+		# Ask the user what to do with the error
+		choice = input("[A]bort, [C]hange port, or [R]etry?");
+
+		if(choice.lower() == "a"):
+			exit();
+		elif(choice.lower() == "c"):
+			port_number = input("Please enter the port:");
+
+
+def clientThread(connection):
+	# Send the welcome message back to the client
+	connection.send("Welcome to Tic Tac Toe online\n".encode());
+
+	while True:
+		# Receive message from the connected client with message size being 1024 bytes
+		data_in = connection.recv(1024);
+
+		# Decode the message being received
+		message = data_in.decode();
+
+		# If the client sends 'q', then break the loop and disconnect
+		if(message == "q"):
+			break;
+
+		# Print the message onto the screen
+		print("Received message:", message);
+
+		# Send the data back to the client
+		connection.send(message.encode());
+
+while True:
+	# Accept a connection from a client
+	connection, client_address = server_socket.accept();
+	print("Received connection from ", client_address);
+
+	threading.Thread(target=clientThread, args=(connection,)).start();
+
+# Close the socket
+server_socket.close();
