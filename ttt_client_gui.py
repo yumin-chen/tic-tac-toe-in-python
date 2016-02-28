6ab55afef9d619dea515c50e14c9f9e0eaf7ce73
CharmySoft <charmysoft@gmail.com>
2016-02-28 12:25:16 +0000

    Create a CanvasButton class

--- ttt_client_gui.py
+++ ttt_client_gui.py
@@ -12,6 +12,70 @@ C_COLOR_BLUE_LIGHT = "#e4f1fe";
 C_COLOR_BLUE_DARK = "#304e62";
 C_COLOR_BLUE = "#a8d4f2";
 
+class CanvasButton:
+
+	# Define constant width and height
+	WIDTH = 196;
+	HEIGHT = 32;
+
+	# Count the number of buttons objects initialized
+	count = 0;
+
+	def __init__(self, canvas, x, y, button_text):
+		self.canvas = canvas;
+		# Generate a unique id for each button (for tags)
+		self.id = str(CanvasButton.count);
+		CanvasButton.count = CanvasButton.count + 1;
+
+		# Create the rectangle background
+		rect = canvas.create_rectangle(x - self.WIDTH/2 + self.HEIGHT/2, y - self.HEIGHT/2, x + self.WIDTH/2 - self.HEIGHT/2, y + self.HEIGHT/2, fill=C_COLOR_BLUE, outline="", tags=("btn_bg" + self.id, "rect" + self.id));
+		# Create the two circles on both sides to create a rounded edge
+		oval_l = canvas.create_oval(x - self.WIDTH/2, y - self.HEIGHT/2, x - self.WIDTH/2 + self.HEIGHT, y + self.HEIGHT/2, fill=C_COLOR_BLUE, outline="", tags=("btn_bg" + self.id, "oval_l" + self.id));
+		oval_r = canvas.create_oval(x + self.WIDTH/2 - self.HEIGHT, y - self.HEIGHT/2, x + self.WIDTH/2, y + self.HEIGHT/2, fill=C_COLOR_BLUE, outline="", tags=("btn_bg" + self.id, "oval_r" + self.id));
+		# Create the button text
+		canvas.create_text(x, y, font="Helvetica 16 bold", text=button_text, fill=C_COLOR_BLUE_DARK);
+		# Bind events
+		canvas.tag_bind("rect" + self.id, "<Enter>", self.btn_on_enter_rect);
+		canvas.tag_bind("oval_l" + self.id, "<Enter>", self.btn_on_enter_oval_l);
+		canvas.tag_bind("oval_r" + self.id, "<Enter>", self.btn_on_enter_oval_r);
+		canvas.tag_bind("btn_bg" + self.id, "<Leave>", self.btn_on_leave);
+		canvas.tag_bind("rect" + self.id, "<Leave>", self.btn_on_leave_rect);
+		canvas.tag_bind("oval_l" + self.id, "<Leave>", self.btn_on_leave_oval_l);
+		canvas.tag_bind("oval_r" + self.id, "<Leave>", self.btn_on_leave_oval_r); 
+		# Create a dictionary used to mark when a component is hovered 
+		self.entered = {"rect" + self.id: False, "oval_l" + self.id: False, "oval_r" + self.id: False};
+
+	def btn_on_enter(self, event):
+		self.canvas.itemconfig("btn_bg" + self.id, fill=C_COLOR_BLUE_DARK);
+
+	def btn_on_enter_rect(self, event):
+		self.entered["rect" + self.id] = True;
+		self.btn_on_enter(event);
+
+	def btn_on_enter_oval_l(self, event):
+		self.entered["oval_l" + self.id] = True;
+		self.btn_on_enter(event);
+
+	def btn_on_enter_oval_r(self, event):
+		self.entered["oval_r" + self.id] = True;
+		self.btn_on_enter(event);
+
+	def btn_on_leave(self, event):
+		if(self.entered["rect" + self.id] == False and self.entered["oval_l" + self.id] == False and self.entered["oval_r" + self.id] == False):
+			self.canvas.itemconfig("btn_bg" + self.id, fill=C_COLOR_BLUE);
+
+	def btn_on_leave_rect(self, event):
+		self.entered["rect" + self.id] = False;
+		self.btn_on_leave(event);
+
+	def btn_on_leave_oval_l(self, event):
+		self.entered["oval_l" + self.id] = False;
+		self.btn_on_leave(event);
+
+	def btn_on_leave_oval_r(self, event):
+		self.entered["oval_r" + self.id] = False;
+		self.btn_on_leave(event);
+
 # Define a subclass of Canvas as an abstract base scene class
 class BaseScene(tkinter.Canvas):
 
@@ -63,17 +127,19 @@ class WelcomeScene(BaseScene):
 		self.addtag_all("all");
 
 	def create_button(self, x, y, button_text):
-		# Define constant width and height
-		w = 196;
-		h = 32;
-		# Create the rectangle background
-		self.create_rectangle(x - w/2 + h/2, y - h/2, x + w/2 - h/2, y + h/2, fill=C_COLOR_BLUE, outline="");
-		# Create the two circles on both sides to create a rounded edge
-		self.create_oval(x - w/2, y - h/2, x - w/2 + h, y + h/2, fill=C_COLOR_BLUE, outline="");
-		self.create_oval(x + w/2 - h, y - h/2, x + w/2, y + h/2, fill=C_COLOR_BLUE, outline="");
-		# Create the button text
-		self.create_text(x, y, font="Helvetica 16 bold", text=button_text, fill=C_COLOR_BLUE_DARK);
+		return CanvasButton(self, x, y, button_text);
 
+# Define a subclass of BaseScene for the about scene
+class AboutScene(BaseScene):
+
+	def __init__(self, parent):
+		BaseScene.__init__(self, parent);
+
+		# Create a blue arch at the bottom of the canvas
+		self.create_arc((-64, C_WINDOW_HEIGHT - 192, C_WINDOW_WIDTH + 64, C_WINDOW_HEIGHT + 368), start=0, extent=180, fill=C_COLOR_BLUE, outline="");
+
+		# Tag all of the drawn widgets for later reference
+		self.addtag_all("all");
 
 # Create a Tkinter object
 root = tkinter.Tk();
@@ -93,9 +159,15 @@ except:
 
 # Initialize the welcome scene
 welcome_scene = WelcomeScene(root);
-
 # Pack the welcome scene
 welcome_scene.pack();
+
+# Initialize the about scene
+about_scene = AboutScene(root);
+# Pack the about scene
+about_scene.pack();
+# Give a reference to the welcome scene for switching between scenes
+welcome_scene.about_scene = about_scene;
     
 # Main loop
 root.mainloop();
