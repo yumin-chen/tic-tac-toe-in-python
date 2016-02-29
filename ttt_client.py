9c643f43c0d92ec56a404af4bd99467b4d11d3f3
CharmySoft <charmysoft@gmail.com>
2016-02-29 19:22:49 +0000

    Add self-documentation strings for each function
    The old code has been re-organized and put into different classes.

--- ttt_client.py
+++ ttt_client.py
@@ -3,6 +3,231 @@ import socket
 # Import command line arguments
 from sys import argv
 
+class TTTClient:
+	"""TTTClient deals with networking and communication with the TTTServer."""
+
+	def __init__(self):
+		"""Initializes the client and create a client socket."""
+		# Create a TCP/IP socket
+		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+	def connect(self, address, port_number):
+		"""Keeps repeating connecting to the server and returns True if 
+		connected successfully."""
+		while True:
+			try:
+				print("Connecting to the game server...");
+				# Connect to the specified host and port 
+				self.client_socket.connect((address, int(port_number)));
+				# Return True if connected successfully
+				return True;
+			except:
+				# Caught an error
+				print("There is an error when trying to connect to " + str(address) + "::" + str(port_number));
+				# Ask the user what to do with the error
+				choice = input("[A]bort, [C]hange address and port, or [R]etry?");
+				if(choice.lower() == "a"):
+					exit();
+				elif(choice.lower() == "c"):
+					address = input("Please enter the address:");
+					port_number = input("Please enter the port:");
+		return False;
+
+	def connection_lost(self):
+		"""Call this function when the connection is lost."""
+		print("Error: connection lost.");
+		try:
+			# Try and send a message back to the server to notify connection lost
+			self.client_socket.send("q".encode());
+		except:
+			pass;
+		# Raise an error to finish 
+		raise Exception;
+
+	def s_send(self, command_type, msg):
+		"""Sends a message to the server with an agreed command type token 
+		to ensure the message is delivered safely."""
+		# A 1 byte command_type character is put at the front of the message as a communication convention
+		try:
+			self.client_socket.send((command_type + msg).encode());
+		except:
+			# If any error occurred, the connection might be lost
+			self.connection_lost();
+
+	# Safe communication convention to receive message
+	def s_recv(self, size, expected_type):
+		"""Receives a packet with specified size from the server and check 
+		its integrity by comparing its command type token with the expected
+		one."""
+		try:
+			msg = self.client_socket.recv(size).decode();
+			# If received a quit signal from the server
+			if(msg[0] == "Q"):
+				why_quit = "";
+				try:
+					# Try receiving the whole reason why the client has quit
+					why_quit = self.client_socket.recv(1024).decode();
+				except:
+					pass;
+				# Print the resaon
+				print(msg[1:] + why_quit);
+				# Connection lost
+				self.connection_lost();
+			# If the command type token is not the expected type
+			elif(msg[0] != expected_type):
+				# Connection lost
+				self.connection_lost();
+			# If received an integer from the server
+			elif(msg[0] == "I"):
+				# Return the integer
+				return int(msg[1:]);
+			# In other case
+			else:
+				# Return the message
+				return msg[1:];
+			# Simply return the raw message if anything unexpected happended because it shouldn't matter any more
+			return msg;
+		except:
+			# If any error occurred, the connection might be lost
+			self.connection_lost();
+		return None;
+
+	def close(self):	
+		"""Shut down the socket and close it"""
+		# Shut down the socket to prevent further sends/receives
+		self.client_socket.shutdown(socket.SHUT_RDWR);
+		# Close the socket
+		self.client_socket.close();
+
+class TTTClientGame(TTTClient):
+	"""TTTClientGame deals with the game logic on the client side."""
+
+	def __init__(self):
+		"""Initializes the client game object."""
+		TTTClient.__init__(self);
+
+	def start_game(self):
+		"""Starts the game and gets basic game information from the 
+		server."""
+		# Wrap the whole game logic with a gigantic try and catch to handle the errors gracefully
+		try:
+			# Receive the player's ID from the server
+			self.player_id = int(self.s_recv(128, "A"));
+			# Confirm the ID has been received
+			self.s_send("c","1");
+
+			print("Welcome to Tic Tac Toe online, player " + str(self.player_id) 
+				+ "\nPlease wait for another player to join the game...");
+
+			# Receive the assigned role from the server
+			self.role = str(self.s_recv(2, "R"));
+			# Confirm the assigned role has been received
+			self.s_send("c","2");
+
+			# Receive the mactched player's ID from the server
+			self.match_id = int(self.s_recv(128, "I"));
+			# Confirm the mactched player's ID has been received
+			self.s_send("c","3");
+
+			print(("You are now matched with player " + str(self.match_id) 
+				+ "\nYou are the \"" + self.role + "\""));
+
+			# Go to the main loop
+			self.main_loop();
+		except:
+			print(("Game finished unexpectedly!"));
+
+	def main_loop(self):
+		"""The main game loop."""
+		while True:
+			# Get the board content from the server
+			board_content = self.s_recv(10, "B");
+			# Get the command from the server 
+			command = self.s_recv(2, "C");
+
+			if(command == "Y"):
+				# If it's this player's turn to move, print out the current 
+				# board with " " converted to the corresponding position number
+				print("Current board:\n" + TTTClientGame.format_board(
+					TTTClientGame.show_board_pos(board_content)));
+			else:
+				# Print out the current board
+				print("Current board:\n" + TTTClientGame.format_board(
+					board_content));
+
+			if(command == "Y"):
+				# If it's this player's turn to move
+				while True:
+					# Prompt the user to enter a position
+					position = int(input('Please enter the position (1~9):'));
+					# Ensure user-input data is valid
+					if(position >= 1 and position <= 9):
+						# If the position is between 1 and 9
+						if(board_content[position - 1] != " "):
+							# If the position is already been taken, print out a warning
+							print("That position has already been taken. Please choose another one.");
+						else:
+							# If the user input is valid, break the loop
+							break;
+					else:
+						print("Please enter a value between 1 and 9 that corresponds to the position on the grid board.");
+					# Loop until the user enters a valid value
+
+				# Send the position back to the server
+				self.s_send("i", str(position));
+			elif(command == "N"):
+				# If the player needs to just wait
+				print("Waiting for the other player to make a move...");
+				# Get the move the other player made from the server 
+				move = self.s_recv(2, "I");
+				print("Your opponent took up number " + str(move));
+			elif(command == "D"):
+				# If the result is a draw
+				print("It's a draw.");
+				break;
+			elif(command == "W"):
+				# If this player wins
+				print("You WIN!");
+				# Break the loop and finish
+				break;
+			elif(command == "L"):
+				# If this player loses
+				print("You lose.");
+				# Break the loop and finish
+				break;
+			else:
+				# If the server sends back anything unrecognizable
+				# Simply print it
+				print("Error: unknown message was sent from the server");
+				# And finish
+				break;
+
+	def show_board_pos(s):
+		"""Converts the empty positions " " (a space) in the board string 
+		to its corresponding position index number."""
+
+		new_s = list("123456789");
+		for i in range(0, 8):
+			if(s[i] != " "):
+				new_s[i] = s[i];
+		return "".join(new_s);
+
+	def format_board(s):
+		"""Formats the grid board."""
+
+		# If the length of the string is not 9
+		if(len(s) != 9):
+			# Then print out an error message
+			print("Error: there should be 9 symbols.");
+			# Throw an error 
+			raise Exception;
+
+		# Draw the grid board
+		#print("|1|2|3|");
+		#print("|4|5|6|");
+		#print("|7|8|9|");
+		return "|" + s[0] + "|" + s[1]  + "|" + s[2] + "|\n" + "|" + s[3] + "|" + s[4]  + "|" + s[5] + "|\n" + "|" + s[6] + "|" + s[7]  + "|" + s[8] + "|\n";
+
 # If there are more than 3 arguments 
 if(len(argv) >= 3):
 	# Set the address to argument 1, and port number to argument 2
@@ -13,195 +238,11 @@ else:
 	address = input("Please enter the address:");
 	port_number = input("Please enter the port:");
 
-# Create the socket object, the first parameter AF_INET is for IPv4 networking, the second identifies the socket type, SOCK_STREAM is for TCP protocal
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-
-# Keep repeating connecting to the server
-while True:
-	try:
-		print("Connecting to the game server...");
-
-		# Connect to the specified host and port 
-		client_socket.connect((address, int(port_number)));
-
-		# Break the while loop if no error is caught
-		break;
-
-	except:
-		# Caught an error
-		print("There is an error when trying to connect to " + str(address) + "::" + str(port_number));
-
-		# Ask the user what to do with the error
-		choice = input("[A]bort, [C]hange address and port, or [R]etry?");
-
-		if(choice.lower() == "a"):
-			exit();
-		elif(choice.lower() == "c"):
-			address = input("Please enter the address:");
-			port_number = input("Please enter the port:");
-
-# Connection lost
-def connection_lost():
-	print("Error: connection lost.");
-	try:
-		# Try and send a message back to the server to notify connection lost
-		client_socket.send("q".encode());
-	except:
-		raise;
-
-# Safe communication convention to send message
-def s_send(command_type, msg):
-	# A 1 byte command_type character is put at the front of the message as a communication convention
-	try:
-		client_socket.send((command_type + msg).encode());
-	except:
-		# If any error occurred, the connection might be lost
-		connection_lost();
-
-# Safe communication convention to receive message
-def s_recv(size, expected_type):
-	try:
-		msg = client_socket.recv(size).decode();
-		# If received a quit signal from the server
-		if(msg[0] == "Q"):
-			# Print the resaon
-			print(msg[1:]);
-			# Connection lost
-			connection_lost();
-		# If the message is not the expected type
-		elif(msg[0] != expected_type):
-			# Connection lost
-			connection_lost();
-		# If received an integer from the server
-		elif(msg[0] == "I"):
-			# Return the integer
-			return int(msg[1:]);
-		# In other case
-		else:
-			# Return the message
-			return msg[1:];
-		# Simply return the raw message if anything unexpected happended because it shouldn't matter any more
-		return msg;
-	except:
-		# If any error occurred, the connection might be lost
-		connection_lost();
-	return None;
-
-# Receive the player's ID from the server
-player_id = int(s_recv(128, "A"));
-# Confirm the ID has been received
-s_send("c","1");
-
-print("Welcome to Tic Tac Toe online, player " + str(player_id) + "\nPlease wait for another player to join the game...");
-
-# Receive the assigned role from the server
-role = str(s_recv(2, "R"));
-# Confirm the assigned role has been received
-s_send("c","2");
-
-# Receive the mactched player's ID from the server
-match_id = int(s_recv(128, "I"));
-# Confirm the mactched player's ID has been received
-s_send("c","3");
-
-print(("You are now matched with player " + str(match_id) + "\nGame is getting started!\nYou are the \"" + role + "\""));
-
-# This functon converts empty board position " " to its corresponding position index
-def convertEmptyBoardPosition(s):
-	new_s = list("123456789");
-	for i in range(0, 8):
-		if(s[i] != " "):
-			new_s[i] = s[i];
-	return "".join(new_s);
-
-# Format the Grid Board
-def formatBoard(s):
-	# If the length of the string is not 9
-	if(len(s) != 9):
-		# Then print out an error message
-		print("Error: there should be 9 symbols.");
-		return "";
-
-	# Draw the grid board
-	#print("|1|2|3|");
-	#print("|4|5|6|");
-	#print("|7|8|9|");
-	return "|" + s[0] + "|" + s[1]  + "|" + s[2] + "|\n" + "|" + s[3] + "|" + s[4]  + "|" + s[5] + "|\n" + "|" + s[6] + "|" + s[7]  + "|" + s[8] + "|\n";
-
-while True:
-
-	# Get the board content from the server
-	board_content = s_recv(10, "B");
-	# Get the command from the server 
-	command = s_recv(2, "C");
-
-	# If it's this player's turn to move
-	if(command == "Y"):
-		# Print out the current board with " " converted to the position number
-		print("Current board:\n" + formatBoard(convertEmptyBoardPosition(board_content)));
-	else:
-		# Print out the current board
-		print("Current board:\n" + formatBoard(board_content));
-
-	# If it's this player's turn to move
-	if(command == "Y"):
-		while True:
-			# Prompt the user to enter a position
-			position = int(input('Please enter the position (1~9):'));
-
-			if(position >= 1 and position <= 9):
-
-				if(board_content[position - 1] != " "):
-					# If the position is already been taken, print out a warning
-					print("That position has already been taken. Please choose another one.");
-
-				else:
-					# If the user input is valid, break the loop
-					break;
-			else:
-				print("Please enter a value between 1 and 9 that corresponds to the position on the grid board.");
-			# Else, loop until the user enters a valid value
-
-		# Send the position back to the server
-		s_send("i", str(position));
-
-	# If the player needs to just wait
-	elif(command == "N"):
-
-		# This player waits the other player to make move
-		print("Waiting for the other player to make a move...");
-
-		# Get the move that the other player made from the server 
-		move = s_recv(2, "I");
-		print("Your opponent took up number " + str(move));
-
-	# If the result is a draw
-	elif(command == "D"):
-		print("It's a draw.");
-		break;
-
-	# If this player wins
-	elif(command == "W"):
-		print("You WIN!");
-		# Break the loop and finish
-		break;
-
-	# If this player loses
-	elif(command == "L"):
-		print("You lose.");
-		# Break the loop and finish
-		break;
-
-	# If the server sends back anything unrecognizable
-	else:
-		# Simply print it
-		print("Error: unknown message was sent from the server");
-		# And finish
-		break;
-
-
-# Shut down the socket to prevent further sends/receives
-client_socket.shutdown(socket.SHUT_RDWR);
-
-# Close the socket
-client_socket.close();
+# Initialize the client object
+client = TTTClientGame();
+# Connect to the server
+client.connect(address, port_number);
+# Start the game
+client.start_game();
+# Close the client
+client.close();
