ec6c0e3bbc9adb91c95358eba4af6ef94d251b37
CharmySoft <charmysoft@gmail.com>
2016-03-22 18:17:31 +0000

    Change project root path to /opt/charmysoft/tttpy
    Shorten package name to tttpy

--- build-deb.sh
+++ build-deb.sh
@@ -3,14 +3,14 @@
 VERSION='1.0'
 PACKAGE_REVISION='1'
 # Make a temperary directory for the debian package
-mkdir tic-tac-toe-online_${VERSION}-${PACKAGE_REVISION}
-chmod 0755 tic-tac-toe-online_${VERSION}-${PACKAGE_REVISION}
-cd tic-tac-toe-online_${VERSION}-${PACKAGE_REVISION}
+mkdir tttpy_${VERSION}-${PACKAGE_REVISION}
+chmod 0755 tttpy_${VERSION}-${PACKAGE_REVISION}
+cd tttpy_${VERSION}-${PACKAGE_REVISION}
 
 # Create the DEBIAN/control file
 mkdir DEBIAN
 cat >> DEBIAN/control << EOM
-Package: tic-tac-toe-online
+Package: tttpy
 Version: ${VERSION}-${PACKAGE_REVISION}
 Section: games 
 Installed-Size: 226
@@ -26,47 +26,45 @@ EOM
 
 # Create 'usr' folder
 mkdir usr
-# Create 'usr/bin/tic-tac-toe-online' script
+# Create 'usr/bin/tttpy' script
 mkdir usr/bin
-cat >> usr/bin/tic-tac-toe-online << EOM
+cat >> usr/bin/tttpy << EOM
 #!/bin/bash
-/usr/share/tic-tac-toe-online/ttt_client_gui.py
+cd /opt/charmysoft/tttpy/
+./ttt_client_gui.py
 EOM
 # Add execution permission
-chmod 0755 usr/bin/tic-tac-toe-online
+chmod 0755 usr/bin/tttpy
 # Create 'usr/share' folder
 mkdir usr/share
 # Create a menu entry
 mkdir usr/share/applications
-cat >> usr/share/applications/tic-toc-toe-online.desktop << EOM
+cat >> usr/share/applications/tttpy.desktop << EOM
 [Desktop Entry]
 Name=Tic-Tac-Toe Online
 Version=${VERSION}
-Exec=tic-tac-toe-online
+Exec=tttpy
 Comment=Simple yet fun noughts and crosses game online
-Icon=/usr/share/pixmaps/tic-tac-toe-online.png
+Icon=/opt/charmysoft/tttpy/res/icon.png
 Type=Application
 Terminal=false
 StartupNotify=true
 Categories=Games;
 EOM
 # Add execution permission
-chmod 0644 usr/share/applications/tic-toc-toe-online.desktop
-# Copy the application icon
-mkdir usr/share/pixmaps
-cp ../icons/icon.png usr/share/pixmaps/tic-tac-toe-online.png
-# Copy all the required files to 'usr/share/tic-tac-toe-online'
-mkdir usr/share/tic-tac-toe-online
-cp ../ttt_client.py usr/share/tic-tac-toe-online
-cp ../ttt_client_gui.py usr/share/tic-tac-toe-online
-cp ../res -r usr/share/tic-tac-toe-online/res
+chmod 0644 usr/share/applications/tttpy.desktop
+# Copy all the required files to 'opt/charmysoft/tttpy'
+mkdir opt && mkdir opt/charmysoft && mkdir opt/charmysoft/tttpy
+cp ../ttt_client.py opt/charmysoft/tttpy
+cp ../ttt_client_gui.py opt/charmysoft/tttpy
+cp ../res -r opt/charmysoft/tttpy/res
 # Add execution permission
-chmod 0755 usr/share/tic-tac-toe-online/ttt_client.py
-chmod 0755 usr/share/tic-tac-toe-online/ttt_client_gui.py
+chmod 0755 opt/charmysoft/tttpy/ttt_client.py
+chmod 0755 opt/charmysoft/tttpy/ttt_client_gui.py
 # Build the debian package
 cd .. 
-chown -R root tic-tac-toe-online_${VERSION}-${PACKAGE_REVISION}
-dpkg-deb --build tic-tac-toe-online_${VERSION}-${PACKAGE_REVISION}
+chown -R root tttpy_${VERSION}-${PACKAGE_REVISION}
+dpkg-deb --build tttpy_${VERSION}-${PACKAGE_REVISION}
 
 # Remove the temperary folder
-rm -r tic-tac-toe-online_${VERSION}-${PACKAGE_REVISION}
+rm -r tttpy_${VERSION}-${PACKAGE_REVISION}
